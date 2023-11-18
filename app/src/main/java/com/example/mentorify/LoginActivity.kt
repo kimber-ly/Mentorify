8dc4a2e36adfc5bd98c7f03a0556273d6bda5e3c
Mohamad Dzikri Arfiansyah <mohamaddzikriarfiansyah@gmail.com>
2023-11-19 01:03:49 +0700

    navbar

--- app/src/main/java/com/example/mentorify/LoginActivity.kt
+++ app/src/main/java/com/example/mentorify/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.example.mentorify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin: Button = findViewById(R.id.login_btn)
+        btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.login_btn -> {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mentorify/MainActivity.kt
+++ app/src/main/java/com/example/mentorify/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.mentorify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.buttom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_mentor -> {
+                    replaceFragment(MentorFragment())
+                    true
+                }
+                R.id.bottom_chat -> {
+                    replaceFragment(QnaFragment())
+                    true
+                }
+                R.id.bottom_bookings -> {
+                    replaceFragment(BookingsFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 }
