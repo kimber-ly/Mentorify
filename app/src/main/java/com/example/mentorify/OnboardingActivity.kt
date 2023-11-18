15ad8139a0365f4d3816bfe961c68efea94f563a
kimberly <kimberly08234@gmail.com>
2023-11-18 14:54:06 +0700

    Ilustrasi & RegisterActivity 18 Nov 2023

--- app/src/main/java/com/example/mentorify/OnboardingActivity.kt
+++ app/src/main/java/com/example/mentorify/OnboardingActivity.kt
@@ -1,16 +1,23 @@
 package com.example.mentorify
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.ramotion.paperonboarding.PaperOnboardingFragment
 import com.ramotion.paperonboarding.PaperOnboardingPage
 import java.util.ArrayList
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var register: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
+        register = findViewById(R.id.get_started_btn)
+        register.setOnClickListener(this)
 
         val fragmentManager = supportFragmentManager
         val paperOnboardingFragment = PaperOnboardingFragment.newInstance(getData())
@@ -24,25 +31,25 @@ class OnboardingActivity : AppCompatActivity() {
     private fun getData(): ArrayList<PaperOnboardingPage>? {
         val page1 = PaperOnboardingPage("Selamat Datang",
             "Selamat datang di Mentorify, tempat dimana pembelajaran menjadi sebuah perjalanan yang penuh inspirasi.",
-            Color.parseColor("#FFFFFFFF"),
+            Color.WHITE,
             R.drawable.onboarding_img_1,
             R.drawable.baseline_waving_hand_24
         )
         val page2 = PaperOnboardingPage("Mentor Professional",
             "Anda terhubung langsung dengan mentor professional untuk membimbing langkah - langkah sukses akademis Anda.",
-            Color.parseColor("#FFFFFFFF"),
+            Color.WHITE,
             R.drawable.onboarding_img_2,
             R.drawable.baseline_school_24
         )
         val page3 = PaperOnboardingPage("Komunitas Mahasiswa",
             "Kami memberikan ruang untuk berbagi pengalaman, bertanya, dan mendapatkan dukungan dari komunitas Mahasiswa.",
-            Color.parseColor("#FFFFFFFF"),
+            Color.WHITE,
             R.drawable.onboarding_img_3,
             R.drawable.baseline_people_24
         )
         val page4 = PaperOnboardingPage("Dapatkan Bimbingan dari para Professional",
             "Aplikasi mentoring kami membantu anda mencapai potensi penuh anda dengan dukungan pribadi dari mentor berpengalaman, kurikulum yang disesuaikan, dan komunitas yang mendukung pertumbuhan Anda secara holistik.",
-            Color.parseColor("#FFFFFFFF"),
+            Color.WHITE,
             R.drawable.onboarding_img_4,
             R.drawable.baseline_thumb_up_24
         )
@@ -54,5 +61,14 @@ class OnboardingActivity : AppCompatActivity() {
         elements.add(page4)
 
         return elements
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.get_started_btn -> {
+                val intent = Intent(this@OnboardingActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
