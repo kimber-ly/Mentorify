e9075e28fccb1a678cae85b82ecad440f2e17637
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2023-11-21 09:28:38 +0700

    Some changes in Bookings Fragment

---
+++ app/src/main/java/com/example/mentorify/vpBokingsAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.mentorify
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class vpBokingsAdapter(fm : FragmentManager) : FragmentStatePagerAdapter(fm){
+
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun getCount()= mFragmentList.size
+
+    override fun getItem(position: Int) = mFragmentList[position]
+
+    override fun getPageTitle(position: Int) = mFragmentTitleList[position]
+
+    fun addFragment(fragment: Fragment, title: String){
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+}
