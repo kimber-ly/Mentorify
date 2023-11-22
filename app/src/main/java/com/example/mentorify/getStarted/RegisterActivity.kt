ffa06d10e483a8d1ba18a2af59b1b53162f2db16
kimberly <kimberly08234@gmail.com>
2023-11-22 18:49:30 +0700

    Register & Login bisa pindah 22 Nov 2023

--- app/src/main/java/com/example/mentorify/getStarted/RegisterActivity.kt
+++ app/src/main/java/com/example/mentorify/getStarted/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mentorify.getStarted.RegisterActivity2
 
@@ -13,7 +14,10 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_register)
 
         val btnRegister: Button = findViewById(R.id.register_btn)
+        val toLogin: TextView = findViewById(R.id.to_login_txt)
+
         btnRegister.setOnClickListener(this)
+        toLogin.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -22,6 +26,11 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
                 R.id.register_btn -> {
                     val intent = Intent(this@RegisterActivity,
                         RegisterActivity2::class.java)
+                    startActivity(intent)
+                }
+                R.id.to_login_txt -> {
+                    val intent = Intent(this@RegisterActivity,
+                        LoginActivity::class.java)
                     startActivity(intent)
                 }
             }
