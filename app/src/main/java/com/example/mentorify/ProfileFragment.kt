409ba7ad55e98da15119807ff2f521acab98946a
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2023-11-25 21:16:26 +0700

    optimize edit profile

--- app/src/main/java/com/example/mentorify/ProfileFragment.kt
+++ app/src/main/java/com/example/mentorify/ProfileFragment.kt
@@ -1,12 +1,16 @@
 package com.example.mentorify
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import com.example.mentorify.Utils.SearchPageSection
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +18,19 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn: LinearLayout = view.findViewById(R.id.datadiri_btn_profile)
+        btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.datadiri_btn_profile){
+            val int = Intent(requireActivity(), Profile_EditDataDiri::class.java)
+            startActivity(int)
+        }
     }
 }
--- app/src/main/java/com/example/mentorify/Profile_EditDataDiri.kt
+++ app/src/main/java/com/example/mentorify/Profile_EditDataDiri.kt
@@ -2,10 +2,36 @@ package com.example.mentorify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class Profile_EditDataDiri : AppCompatActivity() {
+    val genders = arrayOf<String>("Pilih","Laki-Laki", "Perempuan")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_edit_data_diri)
+        val spinner = findViewById<Spinner>(R.id.et_gender_datadiripage)
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, genders)
+        arrayAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
+
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@Profile_EditDataDiri, "selected gender:"+genders[position], Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
 }
