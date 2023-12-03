0e6c7c8d223a81c3471cf0920975fcd6d16cd56b
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2023-12-03 20:48:30 +0700

    Revisi layout pembayaran

--- app/src/main/java/com/example/mentorify/pembayaran.kt
+++ app/src/main/java/com/example/mentorify/pembayaran.kt
@@ -2,10 +2,15 @@ package com.example.mentorify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioGroup
 
 class pembayaran : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pembayaran)
+
+        val rgbuton =findViewById<RadioGroup>(R.id.radioGroup)
+
     }
 }
